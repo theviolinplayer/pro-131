eaedcf44d3682694acc72734a85ce3f00cf2f932
theviolinplayer <119118863+theviolinplayer@users.noreply.github.com>
2022-11-26 20:07:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location("bedroom.html")
+}
+
+function books(){
+    window.location("books.html")
+}
+
+function computer(){
+    window.location("computer.html")
+}
+
+function fruit_basket(){
+    window.location("fruit-basket.html")
+}
+
+function kitchen(){
+    window.location("kitchen.html")
+}
